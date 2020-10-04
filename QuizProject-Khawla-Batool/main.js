f672c9ca5b1b827492d1c910615be99fc4727dc4
khawla ilahi <khawla.ilahi1@gmail.com>
2020-10-04 13:03:20 +0400

    filledQuestions

--- QuizProject-Khawla-Batool/main.js
+++ QuizProject-Khawla-Batool/main.js
@@ -1,23 +1,24 @@
-var questions = [{question:"Which number should come next in the pattern?"+"/n"+"37, 34, 31, 28"
-                  answer:"c.25"
+var questions = [{question:"Which number should come next in the pattern:"+"\n"+"37, 34, 31, 28...",
+                  answer:"c.25",
                   choices : ["a.18","b.26","c.25","d.48"]},
-                  {question:"Find the answer that best completes the analogy:"+"/n"+"Book is to Reading as Fork is to:"
-                  answer:"a.eating"
+                  {question:"Find the answer that best completes the analogy: "+"\n"+  "Book is to Reading as Fork is to:",
+                  answer:"a.eating",
                   choices : [ "a.eating", "b.drawing","c.writing","d.stirring"]},
-                  {question:"Which number should replace the ?  in the pattern?"+"/n"+"2,7,12,17,?,27,32"
-                  answer:"d.22"
+                  {question:"Which number should replace the ?  in the pattern?"+"\n"+"2,7,12,17,?,27,32",
+                  answer:"d.22",
                   choices : ["a.17","b.29","c.31","d.22"]},
-                  {question:"One word in this list doesn't belong to the same group:"+"/n"+"Yen, Pound, Franc, Penny, Mark."
-                  answer:"c.Mark"
+                  {question:"One word in this list doesn't belong to the same group:"+"\n"+"Yen, Pound, Franc, Penny, Mark.",
+                  answer:"c.Mark",
                   choices : ["a.Yen","b.Pound","c.Mark","d.Franc"]} ];
 
 
-function fillQuestion(){
 
-	$("form1").text(questions[0].question);
-for (var i = 0; i < questions.length;i++){
-	$("form1").text(questions[i].question);
-}
+
+$("#q1").text(questions[0].question);
+$("#q2").text(questions[1].question);
+$("#q3").text(questions[2].question);
+$("#q4").text(questions[3].question);
 
 	
-}
+
+
